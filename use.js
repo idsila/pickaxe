bf1261054ad3bc530cffd0781339185217e50a02
idsila <idsila88@gmail.com>
2025-08-31 15:56:31 +0400

    update use.js

--- use.js
+++ use.js
@@ -135,3 +135,74 @@ const pp = {
   },
 
 }
+
+
+
+
+
+// Импортируем библиотеку
+const userAgent = require('user-agents');
+const fs = require('fs');
+
+// Случайный UA, где устройство iPhone
+
+const devices = {
+  'Android': 'arm',
+  'Windows': 'x86',
+  'iOS':'arm'
+}
+const arrMemory = [ 2,4,6 ]
+function createUserAgentAndroid(){
+  const obj = new userAgent((user) => { return user.userAgent.includes("Android"); });
+  const currentPlatform = obj.data.userAgent.split('; ')[1].split(' ')[0];
+  const currentPlatformVersion = obj.data.userAgent.split(';')[1].split(' ')[2];
+  const currentVersion = obj.data.userAgent.split('/')[3].split(' ')[0];
+  const currentDeviceMemory =
+  arrMemory[Math.ceil(Math.random()*arrMemory.length)];
+  obj.data.appCodeName =  obj.data.userAgent.split('/')[0];
+  obj.data.appVersion =  obj.data.userAgent.split('Mozilla/')[1];
+  obj.data.deviceMemory = currentDeviceMemory;
+  obj.data.hardwareConcurrency = currentDeviceMemory+2;
+  obj.data.product =  "Gecko";
+  console.log(currentPlatformVersion)
+  obj.data.setUserAgentOverride = {
+    userAgent: obj.data.userAgent,
+    userAgentMetadata: {
+      fullVersion: currentVersion,
+      platform: currentPlatform,
+      platformVersion: `${currentPlatformVersion ?? '10'}.0.0`,
+      architecture: devices[currentPlatform], 
+      mobile: obj.data.userAgent.includes('Mobile'),
+      brands: [
+        {
+         brand: 'Not;A=Brand',
+         version: '99'
+        },
+        {
+          brand: "Chromium",
+          version: currentVersion.split('.')[0]
+        },
+        {
+          brand: "Google Chrome",
+          version: currentVersion.split('.')[0]
+        }
+      ],
+      
+    }
+  };
+  return obj.data
+}
+//console.log(JSON.stringify(createUserAgentAndroid(), null, 2));
+fs.writeFileSync('obj.json',JSON.stringify(createUserAgentAndroid(), null, 2))
+
+
+
+
+
+// Создаём экземпляр
+//const userAgent = new UserAgent({ deviceCategory: 'mobile', platform: 'Android'});
+
+// Можно и объектом:
+//console.log(userAgent.data);
+//console.log(userAgent.data);
+//console.log(userAgent.data);
